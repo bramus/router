92bf57524919c19531b8e182def0867fc3cb2365
Claudio Santoro <claudio.santoro@ieee.org>
2017-05-31 16:07:34 -0300

    Increased Performance of getRequestedMethod, also storing the method on a variable

--- src/Bramus/Router/Router.php
+++ src/Bramus/Router/Router.php
@@ -45,9 +45,14 @@ class Router
     private $serverBasePath;
 
     /**
-     * @var array The Http Request Headers
+     * @var array The Http Requested Headers
      */
     private $headers = array();
+
+    /**
+     * @var string The Http Requested Method
+     */
+    private $method = '';
 
     /**
      * Store a before middleware route and a handling function to be executed when accessed using one of the specified methods
@@ -229,23 +234,28 @@ class Router
      */
     public function getRequestMethod()
     {
-        // Take the method as found in $_SERVER
-        $method = $_SERVER['REQUEST_METHOD'];
+        // If the Requested Method wasn't stored yet, store it
+        if(empty($this->method)) {
+            // Take the method as found in $_SERVER
+            $method = $_SERVER['REQUEST_METHOD'];
 
-        // If it's a HEAD request override it to being GET and prevent any output, as per HTTP Specification
-        // @url http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.4
-        if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
-            ob_start();
-            $method = 'GET';
-        } // If it's a POST request, check for a method override header
-        elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
-            $headers = $this->getRequestHeaders();
-            if (isset($headers['X-HTTP-Method-Override']) && in_array($headers['X-HTTP-Method-Override'], array('PUT', 'DELETE', 'PATCH'))) {
-                $method = $headers['X-HTTP-Method-Override'];
+            // If it's a HEAD request override it to being GET and prevent any output, as per HTTP Specification
+            // @url http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.4
+            if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
+                ob_start();
+                $method = 'GET';
+            } // If it's a POST request, check for a method override header
+            elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+                $headers = $this->getRequestHeaders();
+                if (isset($headers['X-HTTP-Method-Override']) && in_array($headers['X-HTTP-Method-Override'], array('PUT', 'DELETE', 'PATCH'))) {
+                    $method = $headers['X-HTTP-Method-Override'];
+                }
             }
+
+            return $this->method = $method;
         }
 
-        return $method;
+        return $this->method;
     }
 
     /**
