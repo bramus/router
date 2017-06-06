3c68cb787f3393d0462f46ba80c398c409c3cb18
Claudio Santoro <claudio.santoro@redes.unb.br>
2017-06-06 15:40:45 -0300

    Removed http_response_code(), introduced only on php 5.4

--- src/Bramus/Router/Router.php
+++ src/Bramus/Router/Router.php
@@ -305,7 +305,7 @@ class Router
             if ($this->notFoundCallback && is_callable($this->notFoundCallback)) {
                 call_user_func($this->notFoundCallback);
             } else {
-                http_response_code(404);
+                header("{$_SERVER['SERVER_PROTOCOL']} 404 Not Found");
             }
         } // If a route was handled, perform the finish callback (if any)
         else {
