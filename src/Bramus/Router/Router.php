7b4b9a3aa04fc23a46eefcb03d3b40cb53e8578d
Bramus <bramus@bram.us>
2017-10-06 22:59:42 +0200

    Revert "Increased Performance of getRequestedMethod, also storing the method on a variable"
    This reverts commit 92bf57524919c19531b8e182def0867fc3cb2365.
    
    # Conflicts:
    #	src/Bramus/Router/Router.php

--- src/Bramus/Router/Router.php
+++ src/Bramus/Router/Router.php
@@ -45,14 +45,9 @@ class Router
     private $serverBasePath;
 
     /**
-     * @var array The Http Requested Headers
+     * @var array The Http Request Headers
      */
     private $headers = array();
-
-    /**
-     * @var string The Http Requested Method
-     */
-    private $method = '';
 
     /**
      * @var string Default Controllers Namespace
@@ -239,28 +234,23 @@ class Router
      */
     public function getRequestMethod()
     {
-        // If the Requested Method wasn't stored yet, store it
-        if (empty($this->method)) {
-            // Take the method as found in $_SERVER
-            $method = $_SERVER['REQUEST_METHOD'];
+        // Take the method as found in $_SERVER
+        $method = $_SERVER['REQUEST_METHOD'];
 
-            // If it's a HEAD request override it to being GET and prevent any output, as per HTTP Specification
-            // @url http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.4
-            if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
-                ob_start();
-                $method = 'GET';
-            } // If it's a POST request, check for a method override header
-            elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
-                $headers = $this->getRequestHeaders();
-                if (isset($headers['X-HTTP-Method-Override']) && in_array($headers['X-HTTP-Method-Override'], array('PUT', 'DELETE', 'PATCH'))) {
-                    $method = $headers['X-HTTP-Method-Override'];
-                }
+        // If it's a HEAD request override it to being GET and prevent any output, as per HTTP Specification
+        // @url http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html#sec9.4
+        if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
+            ob_start();
+            $method = 'GET';
+        } // If it's a POST request, check for a method override header
+        elseif ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $headers = $this->getRequestHeaders();
+            if (isset($headers['X-HTTP-Method-Override']) && in_array($headers['X-HTTP-Method-Override'], array('PUT', 'DELETE', 'PATCH'))) {
+                $method = $headers['X-HTTP-Method-Override'];
             }
-
-            return $this->method = $method;
         }
 
-        return $this->method;
+        return $method;
     }
 
     /**
