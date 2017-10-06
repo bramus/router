8caf0faa86feb78d271e3528825230958a9c8c84
Bramus <bramus@bram.us>
2017-10-06 22:35:47 +0200

    Refactor controller defition to only call class_exists once

--- src/Bramus/Router/Router.php
+++ src/Bramus/Router/Router.php
@@ -384,8 +384,12 @@ class Router
                 elseif (stripos($route['fn'], '@') !== false) {
                     // explode segments of given route
                     list($controller, $method) = explode('@', $route['fn']);
+                    // Adjust controller class if namespace has been set
+                    if ($this->getNamespace() !== '') {
+                        $controller = $this->getNamespace() . '\\' . $controller;
+                    }
                     // check if class exists, if not just ignore and check if the class exists on the default namespace
-                    if (class_exists($controller) || class_exists($controller = ($this->getNamespace() . '\\' . $controller))) {
+                    if (class_exists($controller)) {
                         // first check if is a static method, directly trying to invoke it. if isn't a valid static method, we will try as a normal method invocation.
                         if (call_user_func_array(array(new $controller, $method), $params) === false) {
                             // try call the method as an non-static method. (the if does nothing, only avoids the notice)
