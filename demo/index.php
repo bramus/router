a03a257edda0eaa1209da938aa16cefcc913118b
C <alb.mueller@gmail.com>
2018-02-17 11:04:13 +0100

    1. Fix request '/' and basePath = '/index.php'
    2. Checks if Var not empty
    
    3. Optimize search and cut of the querystring
    
    4. Fix -> ONLY LEFT TRIM -> right slash is necessary as seperator between a new Path and a File

--- demo/index.php
+++ demo/index.php
@@ -2,9 +2,7 @@
 
     // In case one is using PHP 5.4's built-in server
     //$filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
-if (php_sapi_name() === 'cli-server' )
-{
-	
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
     return false;
 }
 
