8692190532db269882f83d27cea95d4f22a50da2
Bramus <bramus@bram.us>
2019-02-26 15:30:15 +0100

    🐛 urldecode params extracted from the URL (Fixes #80)

--- src/Bramus/Router/Router.php
+++ src/Bramus/Router/Router.php
@@ -351,10 +351,10 @@ class Router
 
                     // We have a following parameter: take the substring from the current param position until the next one's position (thank you PREG_OFFSET_CAPTURE)
                     if (isset($matches[$index + 1]) && isset($matches[$index + 1][0]) && is_array($matches[$index + 1][0])) {
-                        return trim(substr($match[0][0], 0, $matches[$index + 1][0][1] - $match[0][1]), '/');
+                        return trim(urldecode(substr($match[0][0], 0, $matches[$index + 1][0][1] - $match[0][1])), '/');
                     } // We have no following parameters: return the whole lot
 
-                    return isset($match[0][0]) ? trim($match[0][0], '/') : null;
+                    return isset($match[0][0]) ? trim(urldecode($match[0][0]), '/') : null;
                 }, $matches, array_keys($matches));
 
                 // Call the handling function with the URL parameters if the desired input is callable
